c102d6e5e3bc7dd4028d01381c2e71e29587185b
Rutuja9696 <rutuja.patil@contentstact.com>
2020-11-24 12:29:58 +0530

    added title div to the blogs page

--- blog/src/components/blogTitle.js
+++ blog/src/components/blogTitle.js
@@ -2,7 +2,7 @@ export const blogTitle = (title) => {
   const div = document.createElement("div");
   div.classList.add("blog-title");
   const h1 = document.createElement("h1");
-  h1.src = title;
+  h1.innerHTML = title;
   //   img.alt = "Somealternate descriptn";
   div.appendChild(h1);
   return div;
--- blog/src/main.js
+++ blog/src/main.js
@@ -1,9 +1,22 @@
 import { blogs } from "./data.js";
+
 import { blogImage } from "./components/blogImage.js";
+import { blogTitle } from "./components/blogTitle.js";
+import { blogContent } from "./components/blogContent.js";
 
 window.onload = () => {
   const blogDiv = document.getElementById("root");
+
   const blogImageDiv = blogImage(blogs[0].imageUrl);
   console.log(blogImageDiv);
   blogDiv.appendChild(blogImageDiv);
+
+  const blogTitleDiv = blogTitle(blogs[0].title);
+  console.log(blogTitleDiv);
+  blogDiv.appendChild(blogTitleDiv);
+
+  // const blogDiv = document.getElementById("root");
+  const blogContentDiv = blogContent(blogs[0].content);
+  console.log(blogContentDiv);
+  blogDiv.appendChild(blogContentDiv);
 };
